9f497a2363fbee188502b21d047be5623a86d476
Fernandes <143575492+GabsFns@users.noreply.github.com>
2024-09-06 17:33:45 -0300

    Delete OngVeterinaria directory

--- OngVeterinaria/src/main/java/com/example/OngVeterinaria/repository/AdocaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.OngVeterinaria.repository;
-
-import com.example.OngVeterinaria.model.AdocaoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdocaoRepository extends JpaRepository <AdocaoModel, Long> {
-}
--- OngVeterinaria/src/main/java/com/example/OngVeterinaria/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.OngVeterinaria.repository;
-
-import com.example.OngVeterinaria.model.FuncionarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
-}
